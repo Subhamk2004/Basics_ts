3647737cbc80bb844f619e9ef482a5a5f9592a9b
Subham Kumar <subhamk1978@gmail.com>
2024-08-27 00:19:17 +0530

    changes

---
+++ Intro.ts
@@ -0,0 +1,12 @@
+console.log('Hare Krishna');
+
+
+const user = {
+    name: 'Subham Rahar',
+    age:20,
+    email:'subhamrahar22@gmail.com'
+}
+
+// console.log(user.car); this will already produce squigle line while coding as there's no car property in  user onject
+
+// while compiling ts file it creates a js file which then finally runs
